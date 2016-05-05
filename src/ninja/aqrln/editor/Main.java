1d8d3e48062cbfec48ec1444ed859306f5e826ba
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-05-06 01:59:43 +0300

    Initial commit

---
+++ src/ninja/aqrln/editor/Main.java
@@ -0,0 +1,18 @@
+package ninja.aqrln.editor;
+
+/**
+ * Main class of the application
+ *
+ * @author Alexey Orlenko
+ */
+public class Main {
+
+    /**
+     * Application entry point
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
