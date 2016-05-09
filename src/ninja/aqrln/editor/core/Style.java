67001c6c0eefd7cd2cf62434fa13779ec2c99aa2
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-05-09 07:54:34 +0300

    Fill the Style class

--- src/ninja/aqrln/editor/core/Style.java
+++ src/ninja/aqrln/editor/core/Style.java
@@ -1,7 +1,58 @@
 package ninja.aqrln.editor.core;
 
+import java.awt.*;
+
 /**
  * @author Alexey Orlenko
  */
 public class Style {
+    private Font font;
+    private Color backgroundColor;
+    private Color foregroundColor;
+
+    public static final Font DEFAULT_FONT = new Font(Font.SERIF, Font.PLAIN, 12);
+    public static final Color DEFAULT_FG_COLOR = Color.BLACK;
+    public static final Color DEFAULT_BG_COLOR = Color.WHITE;
+
+    public Style(Font font, Color foregroundColor, Color backgroundColor) {
+        this.font = font;
+        this.backgroundColor = backgroundColor;
+        this.foregroundColor = foregroundColor;
+    }
+
+    public Style(Font font, Color foregroundColor) {
+        this(font, foregroundColor, DEFAULT_BG_COLOR);
+    }
+
+    public Style(Font font) {
+        this(font, DEFAULT_FG_COLOR, DEFAULT_BG_COLOR);
+    }
+
+    public Style() {
+        this(DEFAULT_FONT, DEFAULT_FG_COLOR, DEFAULT_BG_COLOR);
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    public Color getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(Color color) {
+        foregroundColor = color;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color color) {
+        backgroundColor = color;
+    }
 }
