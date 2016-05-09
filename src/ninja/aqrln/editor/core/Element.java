69fee5f0d3117ab3887286985cc1c65c20ad28fe
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-05-10 01:35:36 +0300

    Minor refactor of ninja.aqrln.editor.core.Element

--- src/ninja/aqrln/editor/core/Element.java
+++ src/ninja/aqrln/editor/core/Element.java
@@ -2,17 +2,37 @@ package ninja.aqrln.editor.core;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.List;
 import java.util.ListIterator;
 
 /**
  * @author Alexey Orlenko
  */
 public abstract class Element {
-    protected java.util.List<Element> elements;
+    private List<Element> elements;
+    private Style style;
 
     public abstract void draw(Graphics graphics);
 
     public abstract Dimension getSize();
 
-    public abstract ListIterator<Element> getIterator();
+    public ListIterator<Element> getIterator() {
+        return elements.listIterator();
+    }
+
+    public List<Element> getElements() {
+        return elements;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+
+        for (Element element : elements) {
+            element.setStyle(style);
+        }
+    }
 }
