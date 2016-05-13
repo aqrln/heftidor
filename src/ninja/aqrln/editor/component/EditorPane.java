388c3d7e4799bfa32ec4d1902620faa4d6d9f0df
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-05-13 05:07:28 +0300

    Add EditorPane class

---
+++ src/ninja/aqrln/editor/component/EditorPane.java
@@ -0,0 +1,14 @@
+package ninja.aqrln.editor.component;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+/**
+ * @author Alexey Orlenko
+ */
+public class EditorPane extends JPanel {
+    @Override
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+    }
+}
