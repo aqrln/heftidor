77b3ecdf772a98ab0e785fd90833876de25dbd81
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-05-13 05:06:38 +0300

    Add ParagraphElement

---
+++ src/ninja/aqrln/editor/dom/model/ParagraphElement.java
@@ -0,0 +1,15 @@
+package ninja.aqrln.editor.dom.model;
+
+import ninja.aqrln.editor.dom.core.CompositeElement;
+
+import java.awt.Dimension;
+
+/**
+ * @author Alexey Orlenko
+ */
+public class ParagraphElement extends CompositeElement {
+    @Override
+    public Dimension getSize() {
+        return null;
+    }
+}
